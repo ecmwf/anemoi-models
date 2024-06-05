8b63544ef39cf3b50ddaed077c26ebe69352c4d1
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2024-06-05 10:03:33 +0000

    fix: code review feedback

--- src/anemoi/models/models/encoder_processor_decoder.py
+++ src/anemoi/models/models/encoder_processor_decoder.py
@@ -19,6 +19,7 @@ from torch import nn
 from torch.distributed.distributed_c10d import ProcessGroup
 from torch.utils.checkpoint import checkpoint
 
+from anemoi.models.data_indices.collection import IndexCollection
 from anemoi.models.distributed.shapes import get_shape_shards
 from anemoi.models.layers.graph import TrainableTensor
 
@@ -32,7 +33,7 @@ class AnemoiModelEncProcDec(nn.Module):
         self,
         *,
         config: DotConfig,
-        data_indices: dict,
+        data_indices: IndexCollection,
         graph_data: dict,
     ) -> None:
         """Initializes the graph neural network.
@@ -41,12 +42,13 @@ class AnemoiModelEncProcDec(nn.Module):
         ----------
         config : DictConfig
             Job configuration
+        data_indices : IndexCollection
+            Data indices
         graph_data : dict
             Graph definition
         """
         super().__init__()
 
-        self._graph_data = graph_data
         self._graph_name_hidden = config.graphs.hidden_mesh.name
         self._graph_mesh_names = [name for name in graph_data if isinstance(name, str)]
         self._graph_input_meshes = [
@@ -61,11 +63,13 @@ class AnemoiModelEncProcDec(nn.Module):
 
         self.multi_step = config.training.multistep_input
 
-        self._define_tensor_sizes(config)
+        self._define_tensor_sizes(config, graph_data)
+
+        self._create_trainable_attributes()
 
         # Register lat/lon
-        for name in self._graph_mesh_names:
-            self._register_latlon(name)
+        for mesh_key in self._graph_mesh_names:
+            self._register_latlon(mesh_key, graph_data[mesh_key]["coords"])
 
         self.num_channels = config.model.num_channels
 
@@ -73,15 +77,15 @@ class AnemoiModelEncProcDec(nn.Module):
 
         # Encoder data -> hidden
         self.encoders = nn.ModuleDict()
-        for data in self._graph_input_meshes:
-            self.encoders[data] = instantiate(
+        for in_mesh in self._graph_input_meshes:
+            self.encoders[in_mesh] = instantiate(
                 config.model.encoder,
-                in_channels_src=input_dim + self.num_node_features[data] + self.num_trainable_params[data],
+                in_channels_src=input_dim + self.num_node_features[in_mesh] + self.num_trainable_params[in_mesh],
                 in_channels_dst=self.num_node_features[self._graph_name_hidden]
                 + self.num_trainable_params[self._graph_name_hidden],
                 hidden_dim=self.num_channels,
-                sub_graph=self._graph_data[(data, "to", self._graph_name_hidden)],
-                src_grid_size=self.num_nodes[data],
+                sub_graph=graph_data[(in_mesh, "to", self._graph_name_hidden)],
+                src_grid_size=self.num_nodes[in_mesh],
                 dst_grid_size=self.num_nodes[self._graph_name_hidden],
             )
 
@@ -89,23 +93,23 @@ class AnemoiModelEncProcDec(nn.Module):
         self.processor = instantiate(
             config.model.processor,
             num_channels=self.num_channels,
-            sub_graph=self._graph_data.get((self._graph_name_hidden, "to", self._graph_name_hidden), None),
+            sub_graph=graph_data.get((self._graph_name_hidden, "to", self._graph_name_hidden), None),
             src_grid_size=self.num_nodes[self._graph_name_hidden],
             dst_grid_size=self.num_nodes[self._graph_name_hidden],
         )
 
         # Decoder hidden -> data
         self.decoders = nn.ModuleDict()
-        for data in self._graph_output_meshes:
-            self.decoders[data] = instantiate(
+        for out_mesh in self._graph_output_meshes:
+            self.decoders[out_mesh] = instantiate(
                 config.model.decoder,
                 in_channels_src=self.num_channels,
-                in_channels_dst=input_dim + self.num_node_features[data] + self.num_trainable_params[data],
+                in_channels_dst=input_dim + self.num_node_features[out_mesh] + self.num_trainable_params[out_mesh],
                 hidden_dim=self.num_channels,
                 out_channels_dst=self.num_output_channels,
-                sub_graph=self._graph_data[(self._graph_name_hidden, "to", data)],
+                sub_graph=graph_data[(self._graph_name_hidden, "to", out_mesh)],
                 src_grid_size=self.num_nodes[self._graph_name_hidden],
-                dst_grid_size=self.num_nodes[data],
+                dst_grid_size=self.num_nodes[out_mesh],
             )
 
     def _calculate_shapes_and_indices(self, data_indices: dict) -> None:
@@ -126,35 +130,35 @@ class AnemoiModelEncProcDec(nn.Module):
             self._internal_output_idx,
         ), f"Model indices must match {self._internal_input_idx} != {self._internal_output_idx}"
 
-    def _define_tensor_sizes(self, config: DotConfig) -> None:
+    def _define_tensor_sizes(self, config: DotConfig, graph_data: dict) -> None:
         # Define Sizes of different tensors
-        self.num_nodes = {name: self._graph_data[name]["coords"].shape[0] for name in self._graph_mesh_names}
-        self.num_node_features = {
-            name: 2 * self._graph_data[name]["coords"].shape[1] for name in self._graph_mesh_names
-        }
+        self.num_nodes = {name: graph_data[name]["coords"].shape[0] for name in self._graph_mesh_names}
+        self.num_node_features = {name: 2 * graph_data[name]["coords"].shape[1] for name in self._graph_mesh_names}
         self.num_trainable_params = {
-            name: config.model.trainable_parameters.get("data" if name != "hidden" else name, 0)
+            name: config.model.trainable_parameters["data" if name != "hidden" else name]
             for name in self._graph_mesh_names
         }
 
-    def _register_latlon(self, name: str) -> None:
+    def _create_trainable_attributes(self) -> None:
+        """Create all trainable attributes."""
+        self.trainable_tensors = nn.ModuleDict()
+        for mesh in self._graph_mesh_names:
+            self.trainable_tensors[mesh] = TrainableTensor(
+                trainable_size=self.num_trainable_params[mesh], tensor_size=self.num_nodes[mesh]
+            )
+
+    def _register_latlon(self, name: str, coords: torch.Tensor) -> None:
         """Register lat/lon buffers.
 
         Parameters
         ----------
         name : str
             Name of grid to map
+        coords: torch.Tensor
+            Coordinates of the grid
         """
-        trainable_tensor = TrainableTensor(
-            trainable_size=self.num_trainable_params[name], tensor_size=self._graph_data[name]["coords"].shape[0]
-        )
-        setattr(self, f"trainable_{name}", trainable_tensor)
         self.register_buffer(
-            f"latlons_{name}",
-            torch.cat(
-                [torch.sin(self._graph_data[name]["coords"]), torch.cos(self._graph_data[name]["coords"])], dim=-1
-            ),
-            persistent=True,
+            f"latlons_{name}", torch.cat([torch.sin(coords), torch.cos(coords)], dim=-1), persistent=True
         )
 
     def _run_mapper(
@@ -199,40 +203,39 @@ class AnemoiModelEncProcDec(nn.Module):
         )
 
     def forward(self, x: Tensor, model_comm_group: Optional[ProcessGroup] = None) -> Tensor:
-        batch_size, _, ensemble_size, *_ = x.shape
+        batch_size = x.shape[0]
+        ensemble_size = x.shape[2]
 
         # add data positional info (lat/lon)
         x_data_latent = {}
-        for data in self._graph_input_meshes:
-            x_data_latent[data] = torch.cat(
+        for in_mesh in self._graph_input_meshes:
+            x_data_latent[in_mesh] = torch.cat(
                 (
                     einops.rearrange(x, "batch time ensemble grid vars -> (batch ensemble grid) (time vars)"),
-                    getattr(self, f"trainable_{data}")(getattr(self, f"latlons_{data}"), batch_size=batch_size),
+                    self.trainable_tensors[in_mesh](getattr(self, f"latlons_{in_mesh}"), batch_size=batch_size),
                 ),
                 dim=-1,  # feature dimension
             )
 
-        x_hidden_latent = self.trainable_hidden(self.latlons_hidden, batch_size=batch_size)
+        x_hidden_latent = self.trainable_tensors[self._graph_name_hidden](self.latlons_hidden, batch_size=batch_size)
 
         # get shard shapes
-        shard_shapes_data = {}
-        for data in self._graph_input_meshes:
-            shard_shapes_data[data] = get_shape_shards(x_data_latent[data], 0, model_comm_group)
+        shard_shapes_data = {name: get_shape_shards(data, 0, model_comm_group) for name, data in x_data_latent.items()}
         shard_shapes_hidden = get_shape_shards(x_hidden_latent, 0, model_comm_group)
 
         # Run encoders
         x_latents = []
-        for data, encoder in self.encoders.items():
-            x_data_latent[data], x_latent = self._run_mapper(
+        for in_data_name, encoder in self.encoders.items():
+            x_data_latent[in_data_name], x_latent = self._run_mapper(
                 encoder,
-                (x_data_latent[data], x_hidden_latent),
+                (x_data_latent[in_data_name], x_hidden_latent),
                 batch_size=batch_size,
-                shard_shapes=(shard_shapes_data[data], shard_shapes_hidden),
+                shard_shapes=(shard_shapes_data[in_data_name], shard_shapes_hidden),
                 model_comm_group=model_comm_group,
             )
             x_latents.append(x_latent)
 
-        # TODO: This operation can be a desing choice (sum, mean, attention, ...)
+        # TODO: This operation can be a design choice (sum, mean, attention, ...)
         x_latent = torch.stack(x_latents).sum(dim=0) if len(x_latents) > 1 else x_latents[0]
 
         x_latent_proc = self.processor(
@@ -247,18 +250,18 @@ class AnemoiModelEncProcDec(nn.Module):
 
         # Run decoders
         x_out = {}
-        for data, decoder in self.decoders.items():
-            x_out[data] = self._run_mapper(
+        for out_data_name, decoder in self.decoders.items():
+            x_out[out_data_name] = self._run_mapper(
                 decoder,
-                (x_latent_proc, x_data_latent[data]),
+                (x_latent_proc, x_data_latent[out_data_name]),
                 batch_size=batch_size,
-                shard_shapes=(shard_shapes_hidden, shard_shapes_data[data]),
+                shard_shapes=(shard_shapes_hidden, shard_shapes_data[out_data_name]),
                 model_comm_group=model_comm_group,
             )
 
-            x_out[data] = (
+            x_out[out_data_name] = (
                 einops.rearrange(
-                    x_out[data],
+                    x_out[out_data_name],
                     "(batch ensemble grid) vars -> batch ensemble grid vars",
                     batch=batch_size,
                     ensemble=ensemble_size,
@@ -267,7 +270,8 @@ class AnemoiModelEncProcDec(nn.Module):
                 .clone()
             )
 
-            # residual connection (just for the prognostic variables)
-            if data in self._graph_input_meshes:
-                x_out[data][..., self._internal_output_idx] += x[:, -1, :, :, self._internal_input_idx]
+            if out_data_name in self._graph_input_meshes:  # check if the mesh is in the input meshes
+                # residual connection (just for the prognostic variables)
+                x_out[out_data_name][..., self._internal_output_idx] += x[:, -1, :, :, self._internal_input_idx]
+
         return x_out[self._graph_output_meshes[0]]
